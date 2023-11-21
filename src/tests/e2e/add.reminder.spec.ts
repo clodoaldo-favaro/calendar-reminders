f7254bc78e5f2e2ba7a2c5f417aafb663abb1221
Clodoaldo Favaro-Bling <clodoaldo.favero@bling.com.br>
2023-11-20 22:56:24 -0300

    chore: update readme

--- src/tests/e2e/add.reminder.spec.ts
+++ src/tests/e2e/add.reminder.spec.ts
@@ -20,18 +20,3 @@ test('can add reminder to calendar', async ({ page }) => {
 
 	await expect(page.getByText('Study for the test')).toBeVisible()
 })
-
-test('show success toast', async ({ page }) => {
-	await page.getByLabel('Description').fill('Study for the test')
-	await page.getByText('Select a City').click()
-	await page.getByLabel('United States').click()
-	await page.getByLabel('California').click()
-	await page.getByLabel('San Francisco').click()
-	await page.locator('.p-colorpicker-preview').click()
-	await page.locator('.p-colorpicker-color').click()
-	await page.getByRole('main').click()
-
-	await page.getByLabel('Confirm').click()
-
-	await expect(page.getByText('Reminder added')).toBeVisible()
-})
